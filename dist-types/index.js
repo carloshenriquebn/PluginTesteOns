d49dfe1b04fe7c35926b29c9eec773e3962ad45e
Carlos Henrique Barbosa Nascimento <carlos.henriquebn@hotmail.com>
2025-06-18 16:01:58 -0300

    adjust

--- dist-types/index.js
+++ dist-types/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.meuPlugin = exports.rootRouteRef = void 0;
+exports.Meuplugin = exports.rootRouteRef = void 0;
 const core_plugin_api_1 = require("@backstage/core-plugin-api");
 exports.rootRouteRef = (0, core_plugin_api_1.createRouteRef)({
     id: 'Meuplugin',
 });
-exports.meuPlugin = (0, core_plugin_api_1.createPlugin)({
+exports.Meuplugin = (0, core_plugin_api_1.createPlugin)({
     id: 'Meuplugin',
     routes: {
         root: exports.rootRouteRef,
